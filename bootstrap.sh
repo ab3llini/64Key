226cfefecdac49d65ec1b9aa798d809ee9d88985
Niccolò Izzo <n1zzo@users.noreply.github.com>
2018-07-28 23:31:49 +0200

    Add 64key package stub

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,7 @@ git clone https://github.com/openwrt/openwrt.git
 cd openwrt
 cp ../diffconfig .config
 ln -s ../files files
+ln -s ../../../../64key package/network/services
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 make defconfig
